0978d8ce8b38cf3f2b900e4cc2586cf87f6e397c
MicolopoCZ <armymisa@gmail.com>
2024-05-06 11:34:33 +0200

    added about page

---
+++ client/src/pages/About/About.jsx
@@ -0,0 +1,8 @@
+
+export default function About() {
+    return (
+        <>
+            <h1>Clean race!</h1>
+        </>
+    )
+}
--- client/src/pages/AppRoutes.jsx
+++ client/src/pages/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
+import About from "./About/About";
 export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />}/>
+                <Route path="/about" element={<About />}/>
             </Routes>
         </BrowserRouter>
     )
