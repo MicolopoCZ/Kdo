045b0695fe532c949e8fdf50d5f3f29e25a6accf
MicolopoCZ <armymisa@gmail.com>
2024-05-06 11:55:41 +0200

    visual

--- client/src/pages/AppRoutes.jsx
+++ client/src/pages/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
 import Contact from "./Contact/Contact";
+
 export default function AppRoutes() {
     return (
         <BrowserRouter>
